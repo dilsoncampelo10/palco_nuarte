638d642643bd399ff4e8ffb13c9ee9e60c526fa1
dilsoncampelo10 <dilsono316@gmail.com>
2022-12-04 20:08:24 -0300

    Enviando e deletando arquivos

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -7,6 +7,7 @@ use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class ProjectController extends Controller
 {
@@ -27,15 +28,26 @@ class ProjectController extends Controller
         $author = $request->author;
         $date = $request->date;
 
-        if (($category && $title && $description) || ($file && $category)) {
+        if (($category && $title && $description && $author) || ($file && $category && $author && $title)) {
+
+
+            if ($request->hasFile('file') && $request->file('file')->isValid()) {
+
+                $requestFile = $request->file;
+                $extension = $requestFile->extension();
+                $fileName = md5($requestFile->getClientOriginalName() . strtotime(now())) . "." . $extension;
+                $requestFile->move(public_path('assets/projects/user/file'), $fileName);
+                $file = $fileName;
+            }
+
             Project::create([
                 'title' => $title,
                 'description' => $description,
                 'id_category' => $category,
                 'id_user' => Auth::user()->id,
                 'author' => $author,
-                'dateCreation' => $date
-                // 'file' => $request->file
+                'dateCreation' => $date,
+                'file' => $file
             ]);
 
             return redirect()->route('project')->with('flash', 'Projeto enviado! Aguarde sua aprovação...');
@@ -68,6 +80,11 @@ class ProjectController extends Controller
 
     public function destroy($id)
     {
+
+        $project = Project::find($id);
+
+        unlink(public_path("assets/projects/user/file/" . $project->file));
+
         Project::findOrFail($id)->delete();
 
         return redirect()->route('admin.project')->with('flash', 'Projeto recusado!');
