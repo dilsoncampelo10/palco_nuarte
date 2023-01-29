234df079270cc02749f16712955acb3da268c035
Dilson <dilsono316@gmail.com>
2023-01-29 20:36:07 -0300

    deixando notícias prontas

--- resources/views/notice.blade.php
+++ resources/views/notice.blade.php
@@ -20,16 +20,19 @@
 
         <div class="row row-sm row-40 row-md-50">
             @foreach($notices as $notice)
-            <div class="col-sm-6 col-md-12 wow fadeInRight">
+            <div class="col-sm-6 col-md-12 wow fadeInRight" id="notice{{$notice->id}}">
                 <article class="product-big">
                     <div class="unit flex-column flex-md-row align-items-md-stretch">
-                        <div class="unit-left"><a class="product-big-figure" href="#"><img src="/assets/notices/{{$notice->image}}" alt="{{$notice->title}}" width="600" height="800" /></a></div>
+                        <div class="unit-left"><a class="product-big-figure" href="#notice{{$notice->id}}"><img src="/assets/notices/{{$notice->image}}" alt="{{$notice->title}}" width="600" height="800" /></a></div>
                         <div class="unit-body">
                             <div class="product-big-body">
-                                <h5 class="product-big-title"><a href="#">{{$notice->title}}</a></h5>
+                                <h5 class="product-big-title"><a href="#notice{{$notice->id}}">{{$notice->title}}</a></h5>
                                 <div class="group-sm group-middle justify-content-start">
-                                    <div class="product-big-rating"></div><a class="product-big-reviews" href="#"> {{$notice->description}}</a>
+                                    <div class="product-big-rating"></div><a class="product-big-reviews" href="#notice{{$notice->id}}"> {{$notice->description}}</a>
                                 </div>
+                                @if($notice->link)
+                                <a class="button button-black-outline button-ujarak" href="{{$notice->link}}" target="_blank">Visualizar Notícia</a>
+                                @endif
                             </div>
                         </div>
                     </div>
